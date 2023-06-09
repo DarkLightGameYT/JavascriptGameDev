d3c4b981560f2893cd254002248a269c23afbe89
DarkLightGameYT <125415219+DarkLightGameYT@users.noreply.github.com>
2023-06-09 23:22:14 +0000

    Commit -> Friday - June 9, 2023

--- pizzaclicker.js
+++ pizzaclicker.js
@@ -1,3 +1,17 @@
 console.log("Vintage Is A: Vital Individual Noted To Abuse Grammar Errors");
 console.log("Or Would You Prefer: Vital Idiot Noted To Abuse Grammar Errors");
 
+//wait untik the window is loaded before registering the onclick event.
+window.addEventListener('load', registerEvents, false);
+//create global counting variable
+var theCount = 0;
+//Registers onclick events
+function registerEvents(e) {
+    document.getElementById('incrementButton').addEventListener('click', increaseCount, false);
+}
+    
+//Increments theCount and displays result
+function increaseCount(e) {
+    theCount++;
+    document.getElementById('currentCount').innerHTML = theCount;
+}
