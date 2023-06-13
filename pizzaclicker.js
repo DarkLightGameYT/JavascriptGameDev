fe9230845a8995b301a3508ada969bbc87e9f2c7
ArmadaGamingYT <136210948+ArmadaGamingYT@users.noreply.github.com>
2023-06-13 22:12:25 +0000

    Commit -> Tuesday - June 13, 2023 (6:08 PM EST)

--- pizzaclicker.js
+++ pizzaclicker.js
@@ -8,10 +8,19 @@ var theCount = 0;
 //Registers onclick events
 function registerEvents(e) {
     document.getElementById('incrementButton').addEventListener('click', increaseCount, false);
+    document.getElementById('currentPizzas').addEventListener('click', increaseCount, false);
+  //document.getElementById('currentpps').addEventListener('click', increaseCount, false);
+    document.getElementById('pizzasbakedthisascension').addEventListener('click', increaseCount, false);
+    document.getElementById('pizzasbakedalltime').addEventListener('click', increaseCount, false);
 }
+
     
 //Increments theCount and displays result
 function increaseCount(e) {
     theCount++;
     document.getElementById('currentCount').innerHTML = theCount;
+    document.getElementById('pizzasbank').innerHTML = theCount;
+  //document.getElementById('ppscurrent').innerHTML = theCount;
+    document.getElementById('thisAscension').innerHTML = theCount;
+    document.getElementById('alltime').innerHTML = theCount;
 }
